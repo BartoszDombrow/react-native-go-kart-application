ae3ccf0255c6aa93bd645a2556d6d20c664be67d
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-06 11:15:28 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Działa? Działa xD Chyba...</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
