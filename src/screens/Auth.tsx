59d83156a5ef15d548677fb5bb9311ccc42e1f1a
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-07 10:02:31 +0200

    Auth screen

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const Auth = () => {
   return (
-    <View>
-      <Text style={styles.text}>Auth</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.logoWrapper}>
+        <View style={styles.container}>
+          <Image
+            source={require('../assets/images/car.png')}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.logoWrapperText}>
+          <Text style={styles.logoHeader}>Track</Text>
+          <Text style={styles.logoText}>Masters</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -12,7 +23,44 @@ const Auth = () => {
 export default Auth;
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'black',
+  mainContainer: {
+    backgroundColor: '#577399',
+    height: '100%',
+    paddingTop: 20,
+  },
+  logoWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  logoWrapperText: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoHeader: {
+    fontSize: 36,
+    fontFamily: 'Roboto',
+    color: '#F7F7FF',
+  },
+  logoText: {
+    fontSize: 36,
+    color: '#F7F7FF',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 175,
+    height: 175,
+    backgroundColor: '#BDD5EA',
+    borderRadius: 30,
+  },
+  image: {
+    width: 120,
+    height: 120,
+    transform: [{rotate: '-45deg'}],
   },
 });
