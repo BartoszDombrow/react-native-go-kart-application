457ab934a7c1b44495d2c3d972bf14c08bd978d6
Michal Ziolkowski <m.ziolkowski@neurosys.com>
2022-07-13 12:40:09 +0200

    Settings screen

--- src/screens/MainView.tsx
+++ src/screens/MainView.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from 'react-native';
 
+import Settings from "../components/Settings";
+
 const MainView = () => {
     return (
         <View style={{flex: 1, backgroundColor: 'red'}}>
-            <Text>Menu</Text>
+            <Settings />
         </View>
     )
 }
