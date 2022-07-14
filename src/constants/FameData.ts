74804a811e36620e2b168003656a834a34764d27
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-14 09:43:36 +0200

    create halloffame

---
+++ src/constants/FameData.ts
@@ -0,0 +1,26 @@
+export const FameData = [
+  {
+    name: 'Player1',
+    time: 223156,
+  },
+  {
+    name: 'Player2',
+    time: 253156,
+  },
+  {
+    name: 'Player3',
+    time: 213156,
+  },
+  {
+    name: 'Player4',
+    time: 233156,
+  },
+  {
+    name: 'Player5',
+    time: 243156,
+  },
+  {
+    name: 'Player6',
+    time: 223148,
+  },
+];
---
+++ src/screens/HallOfFame.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {FameData} from '../constants/FameData';
+
+function Halloffame() {
+  const compare = (a: any, b: any) => {
+    const fameA = a.time;
+    const fameB = b.time;
+
+    let comparison = 0;
+
+    if (fameA > fameB) {
+      comparison = 1;
+    } else if (fameA < fameB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  console.log(FameData.sort(compare));
+
+  return (
+    <View>
+      <Text>Siema</Text>
+    </View>
+  );
+}
+
+export default Halloffame;
