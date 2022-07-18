ad03b3f7b56e60b73f2163c157c5e7c65b6a50f5
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-18 12:05:42 +0200

    styling delete acc modal

--- src/components/DeleteAccountModal.tsx
+++ src/components/DeleteAccountModal.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {View, Modal, StyleSheet, Text} from 'react-native';
+import {View, Modal, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Colors from '../constants/Colors';
-import SubmitButton from './SubmitButton';
+import Fonts from '../constants/Fonts';
+import {Shadow} from 'react-native-neomorph-shadows-fixes';
 
 const colors = new Colors();
+const fonts = new Fonts();
 
 interface Props {
   deleteAccountVisible: boolean;
@@ -22,8 +24,31 @@ const DeleteAccountModal: React.FC<Props> = ({
       visible={deleteAccountVisible}>
       <View style={styles.container}>
         <View style={styles.modal}>
-          <Text>Jesteś w delete account</Text>
-          <SubmitButton buttonText="EXIT" onPress={deleteAccountHandler} />
+          <Text style={styles.text}>
+            Do you want to delete your account permamently?
+          </Text>
+          <View style={styles.buttonWrapper}>
+            <Shadow useArt style={styles.buttonShadow}>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  onPress={deleteAccountHandler}
+                  activeOpacity={0.75}
+                  style={styles.button}>
+                  <Text style={styles.buttonText}>YES</Text>
+                </TouchableOpacity>
+              </View>
+            </Shadow>
+            <Shadow useArt style={styles.buttonShadow}>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  onPress={deleteAccountHandler}
+                  activeOpacity={0.75}
+                  style={styles.button}>
+                  <Text style={styles.buttonText}>NO</Text>
+                </TouchableOpacity>
+              </View>
+            </Shadow>
+          </View>
         </View>
       </View>
     </Modal>
@@ -45,6 +70,52 @@ const styles = StyleSheet.create({
     width: '95%',
     backgroundColor: colors.lightBlue,
     borderRadius: 50,
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: fonts.secondaryFont,
+    fontSize: 20,
+    color: colors.darkBlue,
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 20,
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+  },
+  buttonShadow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    backgroundColor: colors.lightBlue,
+    height: 55,
+    width: 150,
+    shadowColor: '#000000',
+    shadowOpacity: 0.6,
+    shadowRadius: 10,
+    shadowOffset: {width: 3, height: 3},
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  buttonText: {
+    color: colors.darkBlue,
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: fonts.primaryFont,
+    textShadowColor: 'rgba(0, 0, 0, 0.35)',
+    textShadowOffset: {width: -2, height: 4},
+    textShadowRadius: 10,
   },
 });
 
