0781861b2e873b1a03240585a63c7d77eae28f36
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-18 15:12:05 +0200

    fix

--- i18n.tsx
+++ i18n.tsx
@@ -1,18 +1,18 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18next from 'i18next';
+import {initReactI18next} from 'react-i18next';
 
-import english from './src/constants/languages/en.json';
 import polish from './src/constants/languages/pl.json';
+import english from './src/constants/languages/en.json';
 
 const resources = {
-    en: english,
-    pl: polish
-}
+  en: english,
+  pl: polish,
+};
 
 i18next.use(initReactI18next).init({
-    compatibilityJSON: 'v3',
-    resources,
-    lng: "en"
+  compatibilityJSON: 'v3',
+  resources,
+  lng: 'en',
 });
 
 export default i18next;
