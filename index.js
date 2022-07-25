f6fb5cbfcfc7b869ea9d7a8a7f6658b4a404e301
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-07-25 14:23:32 +0200

    change location of files and change their imports

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import './i18n';
+import './src/i18n/i18n';
 
 AppRegistry.registerComponent(appName, () => App);
